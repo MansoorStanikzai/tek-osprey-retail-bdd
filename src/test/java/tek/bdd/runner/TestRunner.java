95df8fc0ce6f079b0d8a9f7ebe4b7514f8c8bcb9
Mansoor <Mansoor.Stanikai@tekschool.us>
2024-04-25 19:32:36 -0400

    updateJson

--- src/test/java/tek/bdd/runner/TestRunner.java
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "pretty",
                 "html:target/html_report/index.html",
-                "json:target/jsonReport/report.json"
+                "json:target/jsonReports/report.json"
         }
 )
 public class TestRunner {
